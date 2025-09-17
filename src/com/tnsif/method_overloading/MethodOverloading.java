3c95b8daa8a3f48fca64537f9d5a575e5e4bf958
Gudepu Sanjay <gudepusanjay8@gmail.com>
2025-09-17 23:15:36 +0530

    First and Final Commit

---
+++ src/com/tnsif/method_overloading/MethodOverloading.java
@@ -0,0 +1,35 @@
+package com.tnsif.method_overloading;
+
+class Calculator{
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public double add(double a,double b)
+	{
+		return a+b;
+		
+	}
+	public int add(int a, int b,int c)
+	{
+		return a+b+c;
+	}
+	
+}
+
+
+
+public class MethodOverloading {
+
+	public static void main(String[] args) {
+		
+		Calculator obj = new Calculator();
+		System.out.println(obj.add(2, 5));
+		System.out.println(obj.add(2.5, 5.25));
+		System.out.println(obj.add(44,34,21));
+		
+		
+	}
+
+}
