a0493fdacaad861aed930f683193a890b8df9bfe
Gudepu Sanjay <gudepusanjay8@gmail.com>
2025-09-26 14:08:58 +0530

    First and Final Commit

---
+++ src/com/tnsif/list_collection/LinkedHashSetDemo.java
@@ -0,0 +1,23 @@
+package com.tnsif.list_collection;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+
+	public static void main(String[] args) {
+		
+		LinkedHashSet lhs = new LinkedHashSet<>();
+		
+		lhs.add(44);
+		lhs.add("Sanjay");
+		
+		lhs.add(33);
+		lhs.add("Bharath");
+		System.out.println(lhs);
+		
+		lhs.add("Sanjay");
+		lhs.add("Sai Kiran Reddy");
+
+	}
+
+}
