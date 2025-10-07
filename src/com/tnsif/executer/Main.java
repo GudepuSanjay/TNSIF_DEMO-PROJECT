50f3a8b87bc302f6d9ac6dfe9203ae06a5f43f30
Gudepu Sanjay <gudepusanjay8@gmail.com>
2025-10-07 12:39:44 +0530

    main

--- src/com/tnsif/executer/Main.java
+++ src/com/tnsif/executer/Main.java
@@ -3,9 +3,9 @@ import com.tnsif.service.*;
 public class Main {
 
 	public static void main(String[] args) {
-//		CRUD.addEmployee(1122, "Alpha", "AlphaSector", "AlphaDept");
-//		CRUD.addEmployee(1232, "Beta", "BetaSector","BetaDept");
-//		CRUD.addEmployee(3124, "Gamma","GammaSector", "GammaDept");
+		CRUD.addEmployee(1122, "Alpha", "AlphaSector", "AlphaDept");
+		CRUD.addEmployee(1232, "Beta", "BetaSector","BetaDept");
+		CRUD.addEmployee(3124, "Gamma","GammaSector", "GammaDept");
 		
 		CRUD.showEmployees();
 		
